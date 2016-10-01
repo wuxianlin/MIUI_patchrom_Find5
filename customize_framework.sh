0f311bf8be98c83b97d9d1cbd6a2a349afdd8e51
wuxianlin <wuxianlinwxl@gmail.com>
2016-10-01 20:15:41 +0800

    use miui Editor

--- customize_framework.sh
+++ customize_framework.sh
@@ -45,6 +45,8 @@ function applyPatch() {
 if [ $2 = "$BUILD_OUT/framework" ]
 then
     applyPatch "overlay/framework"
+    rm -rf $2/smali/android/widget/Editor*
+    cp -rf $1/smali/android/widget/Editor*.smali $2/smali/android/widget/
 fi
 
 if [ $2 = "$BUILD_OUT/services" ]
